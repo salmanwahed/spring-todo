a0886e23bd148bac466a5a6be07ae426cdf16ab2
Salman M Wahed <salman@surecash.net>
2024-06-02 14:46:53 +0600

    - Updated user create api's error message.

--- src/main/java/com/salmanwahed/todoapp/controller/UserController.java
+++ src/main/java/com/salmanwahed/todoapp/controller/UserController.java
@@ -3,10 +3,15 @@ package com.salmanwahed.todoapp.controller;
 import com.salmanwahed.todoapp.model.User;
 import com.salmanwahed.todoapp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping(value = "api/user")
@@ -15,8 +20,15 @@ public class UserController {
     UserService userService;
 
     @PostMapping(value = "/signup")
-    private User signUp(@RequestBody User user){
-        return userService.saveUser(user);
+    private ResponseEntity<Object> signUp(@RequestBody User user){
+        try {
+            user = userService.saveUser(user);
+            return new ResponseEntity<>(user, HttpStatus.CREATED);
+        }catch (Exception exception) {
+            Map<String, String> error = new HashMap<>();
+            error.put("message", exception.getLocalizedMessage());
+            return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+        }
     }
 
 }
